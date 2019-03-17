8a74d4e44770fde7b4122ca47ce91cd2e11c97e1
egrieshaber <eric.j.grieshaber@gmail.com>
2019-03-17 15:56:45 -0400

    initial commit.
    beginning component setup for Hold website using react-bootstrap

---
+++ src/components/CustomNavbar.jsx
@@ -0,0 +1,20 @@
+ import React, { Component } from 'react'
+ import { Link } from 'react-router-dom';
+ import { Jumbotron, Container, Row, Col, Image, Button } from 'react-bootstrap';
+ import './Home.css'
+
+ export default class Home extends Component {
+   render() {
+     return (
+       <Container>
+          <Jumbotron>
+            <h2>Welcome to Hold</h2>
+            <p>this is where you will try Hold</p>
+          </Jumbotron>
+          <Link to="/about">
+            <Button bsStyle="primary">About</Button>
+          </Link>
+       </Container>
+     )
+   }
+ }
