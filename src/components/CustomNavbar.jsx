6dc4a84d2ec14b285677261e3269fd1a361a4374
egrieshaber <eric.j.grieshaber@gmail.com>
2019-03-17 18:55:12 -0400

    added About, Home, News components.
    unable to compile CustomNavbar in App.jsx at this time

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -1,20 +1,31 @@
- import React, { Component } from 'react'
- import { Link } from 'react-router-dom';
- import { Jumbotron, Container, Row, Col, Image, Button } from 'react-bootstrap';
- import './Home.css'
+import React, { Component } from 'react'
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
- export default class Home extends Component {
-   render() {
-     return (
-       <Container>
-          <Jumbotron>
-            <h2>Welcome to Hold</h2>
-            <p>this is where you will try Hold</p>
-          </Jumbotron>
-          <Link to="/about">
-            <Button bsStyle="primary">About</Button>
-          </Link>
-       </Container>
-     )
-   }
- }
+export default class CustomNavbar extends Component {
+  render() {
+    return (
+      <Navbar default collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/">Hold</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <NavItem eventKey={1} componentClass={Link} to="/">
+              Home
+            </NavItem>
+            <NavItem eventKey={2} componentClass={Link} to="/about">
+              About
+            </NavItem>
+            <NavItem eventKey={3} componentClass={Link} to="/news">
+              News
+            </NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    )
+  }
+}
