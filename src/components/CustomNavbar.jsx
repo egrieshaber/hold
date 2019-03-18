95953d4fb4cd58bf55789a636f959d731d31251f
egrieshaber <eric.j.grieshaber@gmail.com>
2019-03-18 16:43:23 -0400

    adjusting CustomNavbar
    attempting to fix navbar links, also added image to home.jsx

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -11,14 +11,14 @@ export default class CustomNavbar extends Component {
           </Navbar.Brand>
           <Navbar.Toggle />
         <Navbar.Collapse>
-          <Nav pullRight>
+          <Nav>
             <NavItem eventKey={1} componentClass={Link} href="/" to="/">
               Home
             </NavItem>
-            <NavItem eventKey={2} componentClass={Link} to="/about">
+            <NavItem eventKey={2} componentClass={Link} href="/about" to="/about">
               About
             </NavItem>
-            <NavItem eventKey={3} componentClass={Link} to="/news">
+            <NavItem eventKey={3} componentClass={Link} href="/news" to="/news">
               News
             </NavItem>
           </Nav>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,13 +10,19 @@ export default class Home extends Component {
          <Jumbotron>
            <h2>Welcome to Hold</h2>
            <p>this is where you will try Hold</p>
-         </Jumbotron>
-         <Link to="/about">
-           <Button bsStyle="primary">About</Button>
-           <Row></Row>
-           <Col></Col>
-           <Image></Image>
-         </Link>
+           <Link to="/about">
+             <Button variant="primary">About</Button>
+           </Link>
+        </Jumbotron>
+        <Row className="show-grid text-center">
+          <Col xs={12} sm={4} className="tominsk-wrapper">
+            <Image src="assets/tominsk.jpg" circle className="tominsk-pic" />
+            <h3>Greetings!</h3>
+            <p>I'm Alex, and I would love to share some beautiful things with
+            you.  Please try Hold!</p>
+        </Col>
+      </Row>
+
       </Container>
     )
   }
