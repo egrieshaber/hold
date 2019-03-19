f16495c76f59960db418a591629830951b73ce55
egrieshaber <eric.j.grieshaber@gmail.com>
2019-03-19 14:19:53 -0400

    temporary navbar Changes
    added different links, outside of NavItem, to move between home, about,
    news

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import './CustomNavbar.css';
 
 export default class CustomNavbar extends Component {
   render() {
@@ -8,6 +9,8 @@ export default class CustomNavbar extends Component {
       <Navbar default collapseOnSelect>
           <Navbar.Brand>
             <Link to="/">Hold</Link>
+            <Link to="/about">About</Link>
+            <Link to="/news">News</Link>
           </Navbar.Brand>
           <Navbar.Toggle />
         <Navbar.Collapse>
